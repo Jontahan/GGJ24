55651a24699268e2a7de1c0770b01b58ee9ee1a6
Jonathan Jørgensen <jonathan@riddlebit.net>
2024-01-27 15:19:35 +0100

    Gearing

--- Assets/Player/CarBehavior.cs
+++ Assets/Player/CarBehavior.cs
@@ -3,48 +3,58 @@ using UnityEngine;
 
 public class CarBehavior : MonoBehaviour
 {
-    public float acceleration = 1500f;
+    // Car config
+    public float[] gearForces = {0f, 0f, 0f};
+    public float[] gearingThresholds = {0f, 0f, 0f};
+    public float autoGearTime = 0f;
     public float steering = 300f;
     public float brakeForce = 3000f;
+    
+    // Internal engine variables
     private float currentBrakeForce = 0f;
     private float currentSteerAngle = 0f;
-    private float currentSpeed;
+    public float currentSpeed;
     private bool isBraking;
-
     private Rigidbody rb;
+    public int currentGear = 0;
+    private float currentAcceleration = 0f;
+    private float gearTimer = 0f;
 
-
+    // Wheel physics
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
     public WheelCollider wheelBL;
     public WheelCollider wheelBR;
- 
+    
+    // Wheel visuals
     public Transform wheelFLTrans;
     public Transform wheelFRTrans;
     public Transform wheelBLTrans;
     public Transform wheelBRTrans;
-
     public Transform steeringWheel;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentAcceleration = gearForces[currentGear];
     }
 
     void FixedUpdate()
     {
+        UpdateGearing();
         currentSpeed = rb.velocity.magnitude;
+        Debug.Log(currentSpeed + " --- " + currentGear);
 
         isBraking = Input.GetKey(KeyCode.Space);
         currentBrakeForce = isBraking ? brakeForce : 0f;
 
         if (Input.GetKey(KeyCode.W) && !isBraking)
         {
-            rb.AddForce(transform.forward * acceleration * Time.fixedDeltaTime);
+            rb.AddForce(transform.forward * currentAcceleration * Time.fixedDeltaTime);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(-transform.forward * acceleration * 0.5f * Time.fixedDeltaTime);
+            rb.AddForce(-transform.forward * currentAcceleration * 0.5f * Time.fixedDeltaTime);
         }
 
 
@@ -53,8 +63,6 @@ public class CarBehavior : MonoBehaviour
         float turn = Input.GetAxis("Horizontal");
         
         currentSteerAngle = steering * turn;
-        //Debug.Log(currentSteerAngle);
-        //steeringWheel.Rotate (0, currentSteerAngle * Time.deltaTime, 0);
 
         currentSteerAngle *= Mathf.Clamp(currentSpeed / 7f, 0, 1);
         steeringWheel.Rotate(0, currentSteerAngle * Time.deltaTime, 0);
@@ -64,9 +72,6 @@ public class CarBehavior : MonoBehaviour
         {
             currentSteerAngle *= -1f;
         }
-
-        //var rot = steeringWheel.localEulerAngles;
-        //steeringWheel.localEulerAngles = new Vector3(turn, rot.y, rot.z);
         
         transform.Rotate(0, currentSteerAngle * Time.fixedDeltaTime, 0);
     }
@@ -77,6 +82,31 @@ public class CarBehavior : MonoBehaviour
         wheelBLTrans.Rotate (0, wheelBL.rpm / 60 * 360 * Time.deltaTime, 0);
         wheelBRTrans.Rotate (0, wheelBR.rpm / 60 * 360 * Time.deltaTime, 0);
     }
+    private void UpdateGearing()
+    {
+        currentSpeed = rb.velocity.magnitude;
+        
+        if (currentGear < gearForces.Length - 1 && currentSpeed > gearingThresholds[currentGear]) 
+        {
+            gearTimer += Time.deltaTime;
+            if (gearTimer > autoGearTime) 
+            {
+                currentGear += 1;
+                gearTimer = 0;
+            } 
+        }
+        else
+        {
+            gearTimer = 0;
+        }
+        if(currentGear > 0 && currentSpeed < gearingThresholds[currentGear] / 2) 
+        {
+            currentGear -= 1;
+        }
+        
+        currentAcceleration = gearForces[currentGear];
+    }
+
     private void ApplyBraking()
     {
         if (isBraking)
