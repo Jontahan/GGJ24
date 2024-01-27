5607d40ae40423c2742259a5c881749f0c59231d
Christian Templen Grave <christgrave@gmail.com>
2024-01-27 14:06:29 +0100

    new input system

--- Assets/Player/CarBehavior.cs
+++ Assets/Player/CarBehavior.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarBehavior : MonoBehaviour
 {
@@ -12,50 +14,68 @@ public class CarBehavior : MonoBehaviour
 
     private Rigidbody rb;
 
+    private GameControls inputActions;
+
 
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
     public WheelCollider wheelBL;
     public WheelCollider wheelBR;
- 
+
     public Transform wheelFLTrans;
     public Transform wheelFRTrans;
     public Transform wheelBLTrans;
     public Transform wheelBRTrans;
+
+    void Awake()
+    {
+        inputActions = new GameControls();
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void OnEnable()
+    {
+        inputActions.Car.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Car.Disable();
+    }
+
     void FixedUpdate()
     {
         currentSpeed = rb.velocity.magnitude;
 
-        isBraking = Input.GetKey(KeyCode.Space);
+        isBraking = inputActions.Car.Braking.ReadValue<float>() > 0.5f; // more than 50% pressed on gamepad (i think)
         currentBrakeForce = isBraking ? brakeForce : 0f;
 
-        if (Input.GetKey(KeyCode.W) && !isBraking)
-        {
-            rb.AddForce(transform.forward * acceleration * Time.fixedDeltaTime);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(-transform.forward * acceleration * 0.5f * Time.fixedDeltaTime);
-        }
+        float accelerationInput = inputActions.Car.Acceleration.ReadValue<float>();
+
+        // Removed logic for checking for braking, reapply if needed
+        rb.AddForce(transform.forward * accelerationInput * acceleration * Time.fixedDeltaTime);
 
         ApplyBraking();
 
-        float turn = Input.GetAxis("Horizontal");
-        currentSteerAngle = steering * turn;
+        float steeringInput = inputActions.Car.Steering.ReadValue<float>();
+        currentSteerAngle = steering * steeringInput;
         transform.Rotate(0, currentSteerAngle * Time.fixedDeltaTime, 0);
     }
-    void Update() 
+    void Update()
     {
-        wheelFLTrans.Rotate (0, wheelFL.rpm / 60 * 360 * Time.deltaTime, 0);
-        wheelFRTrans.Rotate (0, wheelFR.rpm / 60 * 360 * Time.deltaTime, 0);
-        wheelBLTrans.Rotate (0, wheelBL.rpm / 60 * 360 * Time.deltaTime, 0);
-        wheelBRTrans.Rotate (0, wheelBR.rpm / 60 * 360 * Time.deltaTime, 0);
+        wheelFLTrans.Rotate(0, wheelFL.rpm / 60 * 360 * Time.deltaTime, 0);
+        wheelFRTrans.Rotate(0, wheelFR.rpm / 60 * 360 * Time.deltaTime, 0);
+        wheelBLTrans.Rotate(0, wheelBL.rpm / 60 * 360 * Time.deltaTime, 0);
+        wheelBRTrans.Rotate(0, wheelBR.rpm / 60 * 360 * Time.deltaTime, 0);
+
+        // Log all outputs from input actions
+        Debug.Log(inputActions.Car.Acceleration.ReadValue<float>());
+        Debug.Log(inputActions.Car.Braking.ReadValue<float>());
+        Debug.Log(inputActions.Car.Steering.ReadValue<float>());
     }
     private void ApplyBraking()
     {
--- Assets/Player/PlayerBehavior.cs
+++ Assets/Player/PlayerBehavior.cs
@@ -9,15 +9,48 @@ public class PlayerBehavior : MonoBehaviour
     private float pitch = 0.0f;
     private float yaw = 0.0f;
 
+    private GameControls inputActions;
+
+    void Awake()
+    {
+        inputActions = new GameControls();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update() 
+    private Vector2 smoothInput;
+
+    private Vector2 GetSmoothedInput(Vector2 rawInput)
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        // Apply exponential moving average smoothing
+        // 0.7, 3.7, 7.0 
+        smoothInput = Vector2.Lerp(smoothInput, rawInput, 7f * Time.deltaTime);
+        return smoothInput;
+    }
+
+    void Update()
+    {
+        Vector2 rawInputHead = inputActions.Player.Head.ReadValue<Vector2>();
+        Vector2 rawInputHead2 = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector2 smoothInputHead = GetSmoothedInput(rawInputHead);
+        float mouseX = smoothInputHead[0] * mouseSensitivity * Time.deltaTime;
+        float mouseY = smoothInputHead[1] * mouseSensitivity * Time.deltaTime;
+        // float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        // float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
 
         pitch -= mouseY;
         pitch = Mathf.Clamp(pitch, -maxPitchAngle, maxPitchAngle);
